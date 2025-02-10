60581d777316b27ea028db3548d4157c1863bc4f
KnitnatsnoK <konstantinfeedback@gmail.com>
2025-02-10 20:05:17 +0100

    clear Projects

--- PyCra/Projects/Test Project/Scripts/main.py
+++
@@ -1,5 +0,0 @@
-# You can import necessary modules or functions from .py files in the "Scripts" folder
-
-# This function runs every frame, so don't do any 'while True - loops' if possible
-def tick():
-	pass
